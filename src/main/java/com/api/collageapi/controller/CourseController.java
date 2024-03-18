400302bd8e5c55515c11076150a067df1db21283
anand9773 <aaanand9773@gmail.com>
2024-03-18 05:56:55 +0530

    api is complited

--- src/main/java/com/api/collageapi/controller/CourseController.java
+++ src/main/java/com/api/collageapi/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.api.collageapi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,9 +38,16 @@ public class CourseController {
     return new ResponseEntity<>(allCourse, HttpStatus.OK);
   }
 
-  @PutMapping("/update-course")
+  @PutMapping("/course")
   public ResponseEntity<String> updateCourse(@RequestBody Course course) {
-    Course status = CourseService.upSert(course);
+    String status = courseService.upSert(course);
+    return new ResponseEntity<>(status, HttpStatus.OK);
+
+  }
+
+  @DeleteMapping("/course/{id}")
+  public ResponseEntity<String> deleteCourse(@PathVariable Integer id) {
+    String status = courseService.deleteById(id);
     return new ResponseEntity<>(status, HttpStatus.OK);
   }
 
